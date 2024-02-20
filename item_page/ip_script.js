b5e53ac8c728fcd738afc4886aaddb24842a4844
vilasini <gulkavarsh@gmail.com>
2024-02-20 09:40:25 +0530

    responsive and ready to be forked

---
+++ item_page/ip_script.js
@@ -0,0 +1,38 @@
+
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+
+const header = document.querySelector(".header");
+const scrollLink = document.querySelectorAll(".navbar a");
+Array.from(scrollLink).map((link) => {
+  link.addEventListener("click", (e) => {
+    // Prevent Default
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop - 90;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+    navbar.classList.remove("show");
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > 300) {
+    scrollTop.classList.add("show");
+  } else {
+    scrollTop.classList.remove("show");
+  }
+});
